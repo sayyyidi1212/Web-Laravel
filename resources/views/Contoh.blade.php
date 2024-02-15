c4e89e7ed4c5ecf708e69e6f93d36229e9e96c4a
sayyyidi1212 <saydand7@gmail.com>
2024-02-15 17:13:34 +0700

    first Commit

---
+++ resources/views/Contoh.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+   
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contoh saja</title>
+ <style> 
+ h1 {
+            color: red;
+            text-align: center;
+        }
+</style>
+</head>
+<body>
+   <h1> 
+    saya mohammad sayyidi!!!
+</h1> 
+</body>
+</html>
